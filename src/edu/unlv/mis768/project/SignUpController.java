892c8f4a4e9a72bbf285662fe846f4318af18b0b
Chloe Fletcher <fletcc1@unlv.nevada.edu>
2019-05-07 05:04:19 -0700

    Account Sign Up

--- src/edu/unlv/mis768/project/SignUpController.java
+++ src/edu/unlv/mis768/project/SignUpController.java
@@ -1,13 +1,20 @@
 package edu.unlv.mis768.project;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class SignUpController {
@@ -29,6 +36,109 @@ public class SignUpController {
 
     @FXML
     private TextField userNametxt;
+    
+    // Event listener for Save Button
+    public void saveButtonListener(ActionEvent e) {
+    	// Get user input
+    	String patientIDText = patientIdTxt.getText();
+    	String username = userNametxt.getText();
+    	String password = passWordTxt.getText();
+    	String confirmPassword = confirmPasswordTxt.getText();
+    	
+    	// Check for empty values
+    	if (patientIDText.isEmpty() || username.isEmpty()
+    			|| password.isEmpty() || confirmPassword.isEmpty()) {
+    		
+    		Alert alert = new Alert(AlertType.WARNING);
+    		alert.setTitle("Warning");
+    		alert.setHeaderText("Invalid Sign Up Attempt");
+    		alert.setContentText("All fields are required");
+
+    		alert.showAndWait();
+    	}
+    	else {
+    		// User filled out all fields
+    		// Check if patient exists
+    		int patientID = Integer.parseInt(patientIDText);
+    		
+    		// Create a connection to the database.
+	        Connection conn =
+	               AppointmentDBUtil.getDBConnection();
+	        
+	        try {
+	        	// Create a statement object
+				Statement stmt = conn.createStatement();
+				
+				// Query patient table
+		        String sqlSelect = "SELECT Username FROM " + AppointmentDBConstants.PATIENT_TABLE_NAME
+		        		+ " WHERE " + AppointmentDBConstants.PATIENT_PK_NAME + " = " + patientID;
+		        ResultSet result = stmt.executeQuery(sqlSelect);
+		        
+		        if (result.last()) {
+		        	// Patient exists
+		        	String databaseUsername = result.getString("Username");
+		        	if (databaseUsername != null && databaseUsername.equalsIgnoreCase(username)) {
+		        		// Patient already has an account
+		        		Alert alert = new Alert(AlertType.ERROR);
+			        	alert.setTitle("Error");
+			        	alert.setHeaderText("Account Already Exists");
+			        	alert.setContentText("Please log in to your account");
+
+			        	alert.showAndWait();
+		        	}
+		        	else {
+		        		// Patient does not already have an account
+		        		// Check if passwords match
+		        		if (password.equals(confirmPassword)) {
+		        			// Passwords match: add account
+		        			String sqlInsert = "INSERT INTO " + AppointmentDBConstants.USER_TABLE_NAME
+		        					+ " VALUES('" + username + "','" + password + "','patient')";
+		        			stmt.executeUpdate(sqlInsert);
+		        			
+		        			String sqlUpdate = "UPDATE " + AppointmentDBConstants.PATIENT_TABLE_NAME
+		        					+ " SET username = '" + username + "' WHERE PatientID = " 
+		        					+ patientID;
+		        			stmt.executeUpdate(sqlUpdate);
+		        			
+		        			Alert alert = new Alert(AlertType.INFORMATION);
+				        	alert.setTitle("Success");
+				        	alert.setHeaderText("Account Successfully Created");
+
+				        	alert.showAndWait();
+		        		}
+		        		else {
+		        			// Passwords do not match
+		        			Alert alert = new Alert(AlertType.ERROR);
+				        	alert.setTitle("Error");
+				        	alert.setHeaderText("Passwords do not match");
+				        	alert.setContentText("Please re-enter passwords");
+
+				        	alert.showAndWait();
+		        		}
+		        	}
+		        }
+		        else {
+		        	// Patient does not exist
+		        	AppointmentDBUtil.closeDBConnection(conn);
+		        	// No rows returned: Username does not exist in database
+		        	Alert alert = new Alert(AlertType.ERROR);
+		        	alert.setTitle("Error");
+		        	alert.setHeaderText("Patient ID does not exist");
+		        	alert.setContentText("Contact your doctor's office");
+
+		        	alert.showAndWait();
+		        }
+	        } catch (SQLException ex) {
+				AppointmentDBUtil.closeDBConnection(conn);
+				Alert alert = new Alert(AlertType.ERROR);
+	        	alert.setTitle("Error");
+	        	alert.setHeaderText("Application Error");
+	        	alert.setContentText(ex.getMessage());
+	        	
+	        	alert.showAndWait();
+			}
+    	}
+    }
     
     // Event listener for Cancel Button
     public void cancelButtonListener(ActionEvent e) throws Exception {
